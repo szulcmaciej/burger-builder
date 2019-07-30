2500523e9c52f3c6470292205ae755c7767aa258
Maciek <szulcmaciej1@gmail.com>
2019-07-30 08:15:12 +0200

    localStorage for authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import OrderList from './containers/OrderList/OrderList';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={OrderList} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={OrderList} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    );
+  }
+
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+  onTryAutoSignup: () => dispatch(actions.authCheckState())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,11 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        idToken: authData.idToken,
-        userId: authData.localId,
+        idToken: token,
+        userId: userId,
     };
 };
 
@@ -24,6 +24,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expirationDate');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -53,7 +56,11 @@ export const auth = (email, password, isSignup) => {
             url,
             authData)
             .then(response => {
-                dispatch(authSuccess(response.data));
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('userId', response.data.userId);
+                localStorage.setItem('expirationDate', expirationDate);
+                dispatch(authSuccess(response.data.idToken, response.data.userId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(error => {
@@ -68,3 +75,22 @@ export const authSetRedirectPath = path => {
         path: path
     };
 };
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch (logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate > new Date()) {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getTime() / 1000 - new Date().getTime() / 1000));
+                console.log(expirationDate.getTime() / 1000 - new Date().getTime() / 1000);
+            } else {
+                dispatch(logout());
+            }
+        }
+    }
+}
